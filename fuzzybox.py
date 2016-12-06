c39990973f4eecab33ac17eb3e859cd6a052a521
Nicole Neubarth <nicoleneubarth@gmail.com>
2016-12-06 16:20:18 -0500

    added sigmoid fit functions

--- fuzzybox.py
+++ fuzzybox.py
@@ -1,10 +1,16 @@
 
+from scipy.optimize import leastsq
+import subprocess as sp
+import numpy as np
+
+
+
 def initVidPipeline(vidpath): 
 	'''
 	vidpath: string of video path name
 	returns: video pipeline
 	'''
-	import subprocess as sp
+
 	#name of ffmpeg binary
 	FFMPEG_BIN = "ffmpeg" 
 	command = [ FFMPEG_BIN,
@@ -34,7 +40,7 @@ def readFrame(pipe,xdim,ydim):
 	ydim: int indicating y dimensions of video
 	returns: 2D image of frame as a numpy array
 	'''
-	import numpy as np
+
 	raw_image = pipe.stdout.read(xdim*ydim*3)
 	image =  np.fromstring(raw_image, dtype='uint8')
 	if not image.size:
@@ -45,3 +51,39 @@ def readFrame(pipe,xdim,ydim):
 	image = image[:,:,1]
 	pipe.stdout.flush()
 	return image
+
+def sigmoid(p,x):
+    '''
+    params: p, list of values for the sigmoid function
+    L, L + y0 is y-value of top asymptote
+    x0, x-value of curve's midpoint
+    k, steepness of curve
+    y0, y-value of the bottom asymptote
+    x, numpy array of x values from the data
+    returns: y values of the fit for the data
+    '''
+    #L, L + y0 is y-value of top asymptote
+    #x0, x-value of curve's midpoint
+    #k, steepness of curve
+    #y0, y-value of the bottom asymptote
+    x0,y0,L,k=p
+    y = L / (1 + np.exp(-k*(x-x0))) + y0
+    return y
+
+def residuals(p, x, y):  
+    ''' 
+    params: p, list of values for sigmoid function
+    x and y are numpy arrays of x and y values respectively
+    returns: residuals for use with leastsq function
+    '''
+    return y - sigmoid(p,x) 
+
+def sigmoidFit(x,y):
+    '''
+    params: x and y are numpy arrays of x and y values respectively
+    returns: the parameters p for use with the sigmoid function as a numpy array
+    '''
+    start_sigmoid=(np.median(x),min(y),max(y),1.0)
+    solution= leastsq(residuals,start_sigmoid,args=(x,y)) 
+    print(solution)
+    return solution[0]
