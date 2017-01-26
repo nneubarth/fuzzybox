05f2de0dfe09f2f8dfee7ab4a3a31efc03d3361d
Nicole Neubarth <nicoleneubarth@gmail.com>
2017-01-26 15:28:52 -0500

    added functions for plotting von frey data

--- fuzzybox.py
+++ fuzzybox.py
@@ -54,61 +54,62 @@ def readFrame(pipe,xdim,ydim):
 	return image
 
 def sigmoid(p,x):
-    '''
-    params: p, list of values for the sigmoid function
-    L, L + y0 is y-value of top asymptote
-    x0, x-value of curve's midpoint
-    k, steepness of curve
-    y0, y-value of the bottom asymptote
-    x, numpy array of x values from the data
-    returns: y values of the fit for the data
-    '''
-    #L, L + y0 is y-value of top asymptote
-    #x0, x-value of curve's midpoint
-    #k, steepness of curve
-    #y0, y-value of the bottom asymptote
-    x0,y0,L,k=p
-    y = L / (1 + np.exp(-k*(x-x0))) + y0
-    return y
+	'''
+	params: p, list of values for the sigmoid function
+	L, L + y0 is y-value of top asymptote
+	x0, x-value of curve's midpoint
+	k, steepness of curve
+	y0, y-value of the bottom asymptote
+	x, numpy array of x values from the data
+	returns: y values of the fit for the data
+	'''
+
+	#L, L + y0 is y-value of top asymptote
+	#x0, x-value of curve's midpoint
+	#k, steepness of curve
+	#y0, y-value of the bottom asymptote
+	x0,y0,L,k=p
+	y = L / (1 + np.exp(-k*(x-x0))) + y0
+	return y
 
 def residuals(p, x, y):  
-    ''' 
-    params: p, list of values for sigmoid function
-    x and y are numpy arrays of x and y values respectively
-    returns: residuals for use with leastsq function
-    '''
-    return y - sigmoid(p,x) 
+	''' 
+	params: p, list of values for sigmoid function
+	x and y are numpy arrays of x and y values respectively
+	returns: residuals for use with leastsq function
+	'''
+	return y - sigmoid(p,x) 
 
 def sigmoidFit(x,y):
-    '''
-    params: x and y are numpy arrays of x and y values respectively
-    returns: the parameters p for use with the sigmoid function as a numpy array
-    '''
-    start_sigmoid=(np.median(x),min(y),max(y),1.0)
-    solution= leastsq(residuals,start_sigmoid,args=(x,y)) 
-    print(solution)
-    return solution[0]
+	'''
+	params: x and y are numpy arrays of x and y values respectively
+	returns: the parameters p for use with the sigmoid function as a numpy array
+	'''
+	start_sigmoid=(np.median(x),min(y),max(y),1.0)
+	solution= leastsq(residuals,start_sigmoid,args=(x,y)) 
+	print(solution)
+	return solution
 
 def convVF_DF(df):
 	'''
- 	params: df, pandas data frame obtained from von Frey csv file
- 	returns: new pandas dataframe with von Frey filament numbers converted to grams and coverted from
- 	heading to column, animal IDs and test date removed, withdrawal thresholds in column 'WD' and converted
- 	to fraction out of 10.
- 	'''
- 	forcesdict = {'1.65':.008,'2.36':.02,'2.83':.07,'3.2':.16,'3.61':.4,'3.84':.6,'4.08':1,'4.17':1.4,'4.31':2,'4.56':4}
- 	for f in df.columns[3:]:
-    	force = forcesdict[f]
-    	tempdf = pd.concat([df['Genotype'],df[f]],axis=1)
-    	tempdf['Force'] = force
-    
-    	tempdf.rename(columns={f:'WD'}, inplace=True)
-    	tempdf['WD'] = tempdf['WD'].apply(lambda x: x/10.0)
-    	try:
-        	newdf = pd.concat([newdf,tempdf])
-    	except NameError:
-        	newdf = tempdf
-    return newdf
+	params: df, pandas data frame obtained from von Frey csv file
+	returns: new pandas dataframe with von Frey filament numbers converted to grams and coverted from
+	heading to column, animal IDs and test date removed, withdrawal thresholds in column 'WD' and converted
+	to fraction out of 10.
+	'''
+	forcesdict = {'1.65':.008,'2.36':.02,'2.83':.07,'3.2':.16,'3.61':.4,'3.84':.6,'4.08':1,'4.17':1.4,'4.31':2,'4.56':4}
+	for f in df.columns[3:]:
+		force = forcesdict[f]
+		tempdf = pd.concat([df['Genotype'],df[f]],axis=1)
+		tempdf['Force'] = force
+
+		tempdf.rename(columns={f:'WD'}, inplace=True)
+		tempdf['WD'] = tempdf['WD'].apply(lambda x: x/10.0)
+		try:
+			newdf = pd.concat([newdf,tempdf])
+		except NameError:
+			newdf = tempdf
+	return newdf
 
 def separateByGenotype(df,genotype):
 	'''
@@ -123,12 +124,14 @@ def separateByGenotype(df,genotype):
 		wtc = df[df['Genotype'] == 'TrkBflox/+ or flox']
 		crec = df[df['Genotype'] == 'AdvCre/+; TrkBflox/+']
 		mt = df[df['Genotype'] == 'AdvCre/+; TrkBflox/flox']
+		cfpc = []
 	elif genotype == 'Ret':
 		wtc = df[df['Genotype'] == 'Retflox/+']
-		crec = df[df['Genotype'] == 'RetCFP/flox or CFP/+']
+		cfpc = df[df['Genotype'] == 'RetCFP/flox or CFP/+']
+		crec = df[df['Genotype'] == 'AdvCre/+; Retflox/+']
 		mt = df[df['Genotype'] == 'AdvCre/+; RetCFP/flox']
 	allgenotypes = pd.concat([wtc,crec,mt])
-	return wtc,crec,mt,allgenotypes
+	return wtc,cfpc,crec,mt,allgenotypes
 
 def makeAverageDF(df):
 	'''
@@ -146,16 +149,41 @@ def calcBootstrap(x,y):
 	params: x and y data points as numpy arrays
 	returns: bootstrapped mean sigmoid fit and standard deviation of sigmoid fit as a numpy array
 	'''
-    x_sig = np.linspace(min(x),max(x),num=100)
-    for i in range(1000):
-        samp_indices = np.random.choice(range(0,len(x)),size=(1,len(x)),replace=True, p=None)
-        sampx = x[samp_indices].flatten()
-        sampy = y[samp_indices].flatten()
-        try:
-            ps = np.vstack((ps, sigmoidFit(sampx,sampy)[0].flatten()))
-            all_sigs = np.vstack((all_sigs, sigmoid(sigmoidFit(sampx,sampy)[0],x_sig).flatten()))
-        except NameError:
-            ps = sigmoidFit(sampx,sampy)[0]
-            all_sigs = sigmoid(sigmoidFit(sampx,sampy)[0],x_sig).flatten()
-                
-    return all_sigs.mean(axis=0), all_sigs.std(axis=0)
+	x_sig = np.linspace(min(x),max(x),num=100)
+	for i in range(1000):
+		samp_indices = np.random.choice(range(0,len(x)),size=(1,len(x)),replace=True, p=None)
+		sampx = x[samp_indices].flatten()
+		sampy = y[samp_indices].flatten()
+		try:
+			ps = np.vstack((ps, sigmoidFit(sampx,sampy)[0].flatten()))
+			all_sigs = np.vstack((all_sigs, sigmoid(sigmoidFit(sampx,sampy)[0],x_sig).flatten()))
+		except NameError:
+			ps = sigmoidFit(sampx,sampy)[0]
+			all_sigs = sigmoid(sigmoidFit(sampx,sampy)[0],x_sig).flatten()
+	            
+	return all_sigs.mean(axis=0), all_sigs.std(axis=0)
+
+def plotSigWithFit(average_df,color):
+	plt.semilogx(average_df['Force'],average_df['WD'], 'o')
+	#least-squares sigmoid fit to the data: convert values to numpy arrays
+	force_val = np.array(average_df['Force'])
+	wd_val = np.array(average_df['WD'])
+	sigfit = sigmoidFit(force_val,wd_val)[0]
+	x_sig = np.linspace(min(force_val),max(force_val),num=100)
+	plt.plot(x_sig,sigmoid(sigfit,x_sig),linestyle='--',color=color)
+	return
+
+def plotVF(df,genotype):
+	from scipy.optimize import leastsq
+	wtc,cfpc,crec,mt,allgenotypes = separateByGenotype(df,genotype)
+	plt.figure()
+	wtc_average = makeAverageDF(wtc)
+	crec_average = makeAverageDF(crec)
+	mt_average = makeAverageDF(mt)
+	plotSigWithFit(wtc_average, 'b')
+	plotSigWithFit(crec_average, 'g')
+	plotSigWithFit(mt_average, 'r')
+	if genotype == 'Ret':
+		cfpc_average = makeAverageDF(cfpc)
+		plotSigWithFit(cfpc_average, 'y')
+	return
